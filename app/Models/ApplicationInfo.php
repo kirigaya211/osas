9b39c719eeddbab8e7d1570a939d8d23ddca28ea
kirigaya211 <kirigayarz@gmail.com>
2025-01-02 16:29:57 +0800

    "wrong Models"

--- app/Models/ApplicationInfo.php
+++ app/Models/ApplicationInfo.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Checklist;
+use App\Models\ApplicationCheckListChecklist;
 
 class ApplicationInfo extends Model
 {
