2189b162277209ceb69f1462d98b4c5986c5eb1a
kirigaya211 <kirigayarz@gmail.com>
2025-01-02 16:31:41 +0800

    "wrong import"

--- app/Models/ApplicationInfo.php
+++ app/Models/ApplicationInfo.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\ApplicationCheckListChecklist;
+use App\Models\ApplicationCheckList;
 
 class ApplicationInfo extends Model
 {
